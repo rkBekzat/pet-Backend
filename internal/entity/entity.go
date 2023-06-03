8dcafb77505f855cd743fd7a5d1a2e4730de50d6
rkBekzat <bekzatrakhimbayev@gmail.com>
2023-06-04 02:23:58 +0300

    edited openapi

--- internal/entity/entity.go
+++ internal/entity/entity.go
@@ -43,3 +43,34 @@ type Address struct {
 	ZipPostalCode string `json:"zip_postal_code"`
 	AddressLine   string `json:"address_line"`
 }
+
+type Article struct {
+	ArticleId int
+	Text      string
+	Date      string
+	AddressId int
+	PhotoId   int
+	UserId    int
+	PetId     int
+}
+
+type Found struct {
+	Article
+	FoundId  int
+	Expenses float32
+}
+
+type Lost struct {
+	Article
+	LostId int
+	Reward float32
+}
+
+type ArticleComment struct {
+	Text      string
+	ArticleId int
+}
+
+type Photo struct {
+	PhotoId int
+}
